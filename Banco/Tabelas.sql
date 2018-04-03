ef85567f106441ccd0d7092e7f32299fc2245862
Cristopher Gollmann <cris_w_gollmann@hotmail.com>
2018-04-03 20:38:03 -0300

    Ajustando Arquivos e View nas Normas

--- Banco/Tabelas.sql
+++ Banco/Tabelas.sql
@@ -1,16 +1,3 @@
--- Database: "ProjetoExperimental"
-
--- DROP DATABASE "ProjetoExperimental";
-
-CREATE DATABASE "ProjetoExperimental"
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'Portuguese_Brazil.1252'
-       LC_CTYPE = 'Portuguese_Brazil.1252'
-       CONNECTION LIMIT = -1;
-
-
 -- Table: public."AtualizacaoChamado"
 
 -- DROP TABLE public."AtualizacaoChamado";
--- Banco/View.sql
+++ Banco/View.sql
@@ -1,26 +1,47 @@
-CREATE VIEW cabecalho_chamado AS
-  SELECT "Chamado".data AS c_data,
+-- View: public.descricao_chamado
+
+-- DROP VIEW public.descricao_chamado;
+
+CREATE OR REPLACE VIEW public.descricao_chamado AS 
+ SELECT "Chamado".idchamado AS d_id,
+    "Chamado".descricao AS d_descricao
+   FROM "Chamado";
+
+ALTER TABLE public.descricao_chamado
+  OWNER TO postgres;
+
+-- View: public.cabecalho_chamado
+
+-- DROP VIEW public.cabecalho_chamado;
+
+CREATE OR REPLACE VIEW public.cabecalho_chamado AS 
+ SELECT "Chamado".idchamado AS c_id,
+    "Chamado".data AS c_data,
     "Chamado".prioridade AS c_prioridade,
     "Chamado".situacao AS c_situacao,
     "Usuario".nome AS c_usuario,
     "Cliente".nome AS c_cliente,
     "TipoChamado".descricao AS c_tipo
-   FROM (((("Chamado"
-     JOIN "UsuarioChamado" ON (("UsuarioChamado".idusuario = "Chamado".idusuario)))
-     JOIN "Usuario" ON (("Usuario".idusuario = "UsuarioChamado".idusuario)))
-     JOIN "Cliente" ON (("Cliente".idcliente = "Chamado".idcliente)))
-     JOIN "TipoChamado" ON (("Chamado".idtipo = "TipoChamado".idtipochamado)));
+   FROM "Chamado"
+     JOIN "UsuarioChamado" ON "UsuarioChamado".idusuario = "Chamado".idusuario
+     JOIN "Usuario" ON "Usuario".idusuario = "UsuarioChamado".idusuario
+     JOIN "Cliente" ON "Cliente".idcliente = "Chamado".idcliente
+     JOIN "TipoChamado" ON "Chamado".idtipo = "TipoChamado".idtipochamado;
 
-    CREATE VIEW atualiza_chamado AS
-  SELECT "AtualizacaoChamado".idchamado AS a_id,
+ALTER TABLE public.cabecalho_chamado
+  OWNER TO postgres;
+
+-- View: public.atualiza_chamado
+
+-- DROP VIEW public.atualiza_chamado;
+
+CREATE OR REPLACE VIEW public.atualiza_chamado AS 
+ SELECT "AtualizacaoChamado".idchamado AS a_id,
     "AtualizacaoChamado".descricao AS a_descricao,
     "AtualizacaoChamado".data AS a_data,
     "Usuario".nome AS a_usuario
-   FROM ("AtualizacaoChamado"
-     JOIN "Usuario" ON (("AtualizacaoChamado".idusuario = "Usuario".idusuario)));
+   FROM "AtualizacaoChamado"
+     JOIN "Usuario" ON "AtualizacaoChamado".idusuario = "Usuario".idusuario;
 
-CREATE VIEW descricao_chamado AS
-  SELECT "Chamado".idchamado AS d_id,
-    "Chamado".descricao AS d_descricao
-   FROM "Chamado";
-
+ALTER TABLE public.atualiza_chamado
+  OWNER TO postgres;
