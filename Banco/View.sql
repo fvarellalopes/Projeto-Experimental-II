2a294c2f6d56c5a913cb607b394354bba01695bc
Cristopher Gollmann <cris_w_gollmann@hotmail.com>
2018-04-10 21:47:52 -0300

    Inserido View e Ajustando View Existentes

--- Banco/View.sql
+++ Banco/View.sql
@@ -1,13 +1,16 @@
--- View: public.descricao_chamado
+-- View: public.atualiza_chamado
 
--- DROP VIEW public.descricao_chamado;
+-- DROP VIEW public.atualiza_chamado;
 
-CREATE OR REPLACE VIEW public.descricao_chamado AS 
- SELECT "Chamado".idchamado AS d_id,
-    "Chamado".descricao AS d_descricao
-   FROM "Chamado";
+CREATE OR REPLACE VIEW public.atualiza_chamado AS 
+ SELECT "AtualizacaoChamado".idchamado AS "A_ID",
+    "AtualizacaoChamado".descricao AS "A_Descricao",
+    "AtualizacaoChamado".data AS "A_Data",
+    "Usuario".nome AS "A_Usuario"
+   FROM "AtualizacaoChamado"
+     JOIN "Usuario" ON "AtualizacaoChamado".idusuario = "Usuario".idusuario;
 
-ALTER TABLE public.descricao_chamado
+ALTER TABLE public.atualiza_chamado
   OWNER TO postgres;
 
 -- View: public.cabecalho_chamado
@@ -15,13 +18,13 @@ ALTER TABLE public.descricao_chamado
 -- DROP VIEW public.cabecalho_chamado;
 
 CREATE OR REPLACE VIEW public.cabecalho_chamado AS 
- SELECT "Chamado".idchamado AS c_id,
-    "Chamado".data AS c_data,
-    "Chamado".prioridade AS c_prioridade,
-    "Chamado".situacao AS c_situacao,
-    "Usuario".nome AS c_usuario,
-    "Cliente".nome AS c_cliente,
-    "TipoChamado".descricao AS c_tipo
+ SELECT "Chamado".idchamado AS "C_ID",
+    "Chamado".data AS "C_Data",
+    "Chamado".prioridade AS "C_Prioridade",
+    "Chamado".situacao AS "C_Situacao",
+    "Usuario".nome AS "C_Usuario",
+    "Cliente".nome AS "C_Cliente",
+    "TipoChamado".descricao AS "C_Tipo"
    FROM "Chamado"
      JOIN "UsuarioChamado" ON "UsuarioChamado".idusuario = "Chamado".idusuario
      JOIN "Usuario" ON "Usuario".idusuario = "UsuarioChamado".idusuario
@@ -31,17 +34,31 @@ CREATE OR REPLACE VIEW public.cabecalho_chamado AS
 ALTER TABLE public.cabecalho_chamado
   OWNER TO postgres;
 
--- View: public.atualiza_chamado
+-- View: public.descricao_chamado
 
--- DROP VIEW public.atualiza_chamado;
+-- DROP VIEW public.descricao_chamado;
 
-CREATE OR REPLACE VIEW public.atualiza_chamado AS 
- SELECT "AtualizacaoChamado".idchamado AS a_id,
-    "AtualizacaoChamado".descricao AS a_descricao,
-    "AtualizacaoChamado".data AS a_data,
-    "Usuario".nome AS a_usuario
-   FROM "AtualizacaoChamado"
-     JOIN "Usuario" ON "AtualizacaoChamado".idusuario = "Usuario".idusuario;
+CREATE OR REPLACE VIEW public.descricao_chamado AS 
+ SELECT "Chamado".idchamado AS "D_ID",
+    "Chamado".descricao AS "D_Descricao"
+   FROM "Chamado";
 
-ALTER TABLE public.atualiza_chamado
+ALTER TABLE public.descricao_chamado
+  OWNER TO postgres;
+
+-- View: public.usuarios
+
+-- DROP VIEW public.usuarios;
+
+CREATE OR REPLACE VIEW public.usuarios AS 
+ SELECT "Usuario".nome AS "Usuario",
+    "Usuario".telefone AS "Contato",
+    "Usuario".email AS "E-Mail",
+    "PerfilUsuario".tipo AS "Perfil",
+    "Cliente".nome AS "Cliente"
+   FROM "Usuario"
+     JOIN "PerfilUsuario" ON "Usuario".idperfil = "PerfilUsuario".idperfilusuario
+     JOIN "Cliente" ON "Usuario".idcliente = "Cliente".idcliente;
+
+ALTER TABLE public.usuarios
   OWNER TO postgres;
