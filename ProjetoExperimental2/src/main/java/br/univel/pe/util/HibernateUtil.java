f09835d28b28aba62d564b6219c17fa15462d155
Fernando Varella Lopes <fvarellalopes@gmail.com>
2018-04-11 21:47:26 -0300

    alteracoes para deploy no heroku

--- ProjetoExperimental2/src/main/java/br/univel/pe/util/HibernateUtil.java
+++ ProjetoExperimental2/src/main/java/br/univel/pe/util/HibernateUtil.java
@@ -1,6 +1,8 @@
 package br.univel.pe.util;
 
-
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Properties;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,25 +15,40 @@ import br.univel.pe.entity.Usuario;
 
 public class HibernateUtil {
 	private static SessionFactory sessionFactory;
-	
-	
+
 	public static SessionFactory getSessionFactory() {
-		  if (sessionFactory == null) {
-	            // loads configuration and mappings
-	            Configuration configuration = new Configuration().configure();
-	            ServiceRegistry serviceRegistry
-	                = new StandardServiceRegistryBuilder()
-	                    .applySettings(configuration.getProperties()).build();
-	             
-	            // builds a session factory from the service registry
-	            sessionFactory = configuration.buildSessionFactory(serviceRegistry);           
-	        }
-		  return sessionFactory;
-		
+		if (sessionFactory == null) {
+			// loads configuration and mappings
+			Configuration configuration = new Configuration().configure();
+			Properties properties = configuration.getProperties();
+			if (System.getenv("HEROKU_POSTGRESQL_COPPER_URL") != null) {
+				URI dbUri;
+				try {
+					dbUri = new URI(System.getenv("HEROKU_POSTGRESQL_COPPER_URL"));
+
+					String username = dbUri.getUserInfo().split(":")[0];
+					String password = dbUri.getUserInfo().split(":")[1];
+					String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+					properties.setProperty("hibernate.connection.url", dbUrl);
+					properties.setProperty("hibernate.connection.username", username);
+					properties.setProperty("hibernate.connection.password", password);
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+
+			}
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(properties).build();
+
+			// builds a session factory from the service registry
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		}
+		return sessionFactory;
+
 	}
+
 	public static Session getSession() {
 		return getSessionFactory().openSession();
 	}
-	
-	
+
 }
