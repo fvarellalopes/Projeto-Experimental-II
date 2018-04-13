d6505a201673afb40b25750a0fc3601b1a858a57
Paulo Bueno <phf.bueno@gmail.com>
2018-04-13 16:10:27 -0300

    arquivo modal js

---
+++ frontend/src/app/usuario/modalusuario.component.js
@@ -0,0 +1,10 @@
+function modalusuario() {
+  $('#squarespaceModal').modal('show');
+  $("#squarespaceModal").on('shown.bs.modal', function() {
+
+  });
+}
+
+$("#pesquisarcliente").click(function() {
+    $('#squarespaceModal').modal('show');
+  });
