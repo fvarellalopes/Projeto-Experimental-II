9d7a6b752fe99b48a15b86b5652c61fd79189de8
Fernando Lopes <fernando.lopes@wssim.com.br>
2018-06-22 17:51:15 -0300

    Signed-off-by: Fernando Lopes <fvarellalopes@gmail.com>

--- ProjetoExperimental2/src/main/java/br/univel/pe/entity/Chamado.java
+++ ProjetoExperimental2/src/main/java/br/univel/pe/entity/Chamado.java
@@ -17,142 +17,151 @@ import javax.persistence.OneToMany;
 @Entity
 public class Chamado implements Serializable, IEntidade<Long> {
 
-	private static final long serialVersionUID = 1925003216718679929L;
+    private static final long serialVersionUID = 1925003216718679929L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idChamado;
-	private String descricao;
-	private int prioridade;
-	private String titulo;
-	private Timestamp data;
-	private int situacao;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idChamado;
+    private String descricao;
+    private int prioridade;
+    private String titulo;
+    private Timestamp data;
+    private int situacao;
 
-	@ManyToOne(optional = false, fetch=FetchType.EAGER)
-	private Cliente cliente;
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Cliente cliente;
 
-	@ManyToOne(optional = false, fetch=FetchType.EAGER)
-	private Usuario usuario;
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Usuario usuario;
 
-	@ManyToOne(optional = false, fetch=FetchType.EAGER)
-	private TipoChamado tipoChamado;
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private TipoChamado tipoChamado;
 
-	@OneToMany(mappedBy = "chamado", cascade = CascadeType.ALL, orphanRemoval = true, fetch=FetchType.EAGER)
-	private List<UsuarioChamado> usuarios = new ArrayList<>();
+    @OneToMany(mappedBy = "chamado", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<UsuarioChamado> usuarios = new ArrayList<>();
 
-	public TipoChamado getTipoChamado() {
-		return tipoChamado;
-	}
+    @OneToMany(mappedBy = "chamado", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<AtualizacaoChamado> atualizacoes = new ArrayList<>();
 
-	public void setTipoChamado(TipoChamado tipoChamado) {
-		this.tipoChamado = tipoChamado;
-	}
+    public TipoChamado getTipoChamado() {
+        return tipoChamado;
+    }
 
-	public Long getIdChamado() {
-		return idChamado;
-	}
+    public void setTipoChamado(TipoChamado tipoChamado) {
+        this.tipoChamado = tipoChamado;
+    }
 
-	public void setIdChamado(Long idChamado) {
-		this.idChamado = idChamado;
-	}
+    public Long getIdChamado() {
+        return idChamado;
+    }
 
-	public int getPrioridade() {
-		return prioridade;
-	}
+    public void setIdChamado(Long idChamado) {
+        this.idChamado = idChamado;
+    }
 
-	public void setPrioridade(int prioridade) {
-		this.prioridade = prioridade;
-	}
+    public int getPrioridade() {
+        return prioridade;
+    }
 
-	public String getTitulo() {
-		return titulo;
-	}
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public String getTitulo() {
+        return titulo;
+    }
 
-	public Cliente getCliente() {
-		return cliente;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
+    public Cliente getCliente() {
+        return cliente;
+    }
 
-	public Usuario getUsuario() {
-		return usuario;
-	}
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
+    public Usuario getUsuario() {
+        return usuario;
+    }
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    public String getDescricao() {
+        return descricao;
+    }
 
-	public Long getId() {
-		return getIdChamado();
-	}
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
-	public void setId(Long id) {
-		setIdChamado(id);
-	}
+    public Long getId() {
+        return getIdChamado();
+    }
 
-	public Timestamp getData() {
-		return data;
-	}
+    public void setId(Long id) {
+        setIdChamado(id);
+    }
 
-	public void setData(Timestamp data) {
-		this.data = data;
-	}
+    public Timestamp getData() {
+        return data;
+    }
 
-	public int getSituacao() {
-		return situacao;
-	}
+    public void setData(Timestamp data) {
+        this.data = data;
+    }
 
-	public void setSituacao(int situacao) {
-		this.situacao = situacao;
-	}
+    public int getSituacao() {
+        return situacao;
+    }
 
-	public List<UsuarioChamado> getUsuarios() {
-		return usuarios;
-	}
+    public void setSituacao(int situacao) {
+        this.situacao = situacao;
+    }
 
-	public void setUsuarios(List<UsuarioChamado> usuarios) {
-		this.usuarios = usuarios;
-	}
+    public List<UsuarioChamado> getUsuarios() {
+        return usuarios;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((idChamado == null) ? 0 : idChamado.hashCode());
-		return result;
-	}
+    public void setUsuarios(List<UsuarioChamado> usuarios) {
+        this.usuarios = usuarios;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Chamado other = (Chamado) obj;
-		if (idChamado == null) {
-			if (other.idChamado != null)
-				return false;
-		} else if (!idChamado.equals(other.idChamado))
-			return false;
-		return true;
-	}
-	
-	
+    public List<AtualizacaoChamado> getAtualizacoes() {
+        return atualizacoes;
+    }
+
+    public void setAtualizacoes(List<AtualizacaoChamado> atualizacoes) {
+        this.atualizacoes = atualizacoes;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idChamado == null) ? 0 : idChamado.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Chamado other = (Chamado) obj;
+        if (idChamado == null) {
+            if (other.idChamado != null)
+                return false;
+        } else if (!idChamado.equals(other.idChamado))
+            return false;
+        return true;
+    }
 
 }
